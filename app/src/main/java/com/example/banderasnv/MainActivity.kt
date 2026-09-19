ba038b5e00e33ab0a26e7af9fa749d950de690d4
josue <josue2017ivan@gmail.com>
2026-09-19 16:27:50 -0600

    bandera ya terminada con su escudo

--- app/src/main/java/com/example/banderasnv/MainActivity.kt
+++ app/src/main/java/com/example/banderasnv/MainActivity.kt
@@ -4,12 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.banderasnv.ui.theme.BanderasnvTheme
 
@@ -20,28 +28,37 @@ class MainActivity : ComponentActivity() {
         setContent {
             BanderasnvTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                   BanderaMexico(modifier = Modifier.padding(innerPadding).fillMaxSize())
                 }
             }
         }
     }
 }
 
+@Preview
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    BanderasnvTheme {
-        Greeting("Android")
+fun BanderaMexico(modifier: Modifier = Modifier){
+    Row(modifier = Modifier.fillMaxSize()) {
+        Box(
+            modifier = Modifier.weight(1f).
+            fillMaxHeight().
+            background(Color.Green)
+        )
+        Box(
+            modifier = Modifier.weight(1f).
+            fillMaxHeight().
+            background(Color.White),
+            contentAlignment = Alignment.Center
+        ){
+            Image(
+                painter = painterResource(id = R.drawable.escudo_nacional_mexicano),
+                contentDescription = "imagen del escudo de mexico"
+            )
+        }
+        Box(
+            modifier = Modifier.weight(1f).
+            fillMaxHeight().
+            background(Color.Red)
+        )
     }
 }
