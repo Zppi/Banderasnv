c2d21b843552d7dc81293b354293f432381c457a
josue <josue2017ivan@gmail.com>
2026-09-19 20:44:22 -0600

    primera parte de la bandera de argentina

--- app/src/main/java/com/example/banderasnv/MainActivity.kt
+++ app/src/main/java/com/example/banderasnv/MainActivity.kt
@@ -4,12 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.banderasnv.ui.theme.BanderasnvTheme
 
@@ -20,28 +27,31 @@ class MainActivity : ComponentActivity() {
         setContent {
             BanderasnvTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    BanderaArgentina(modifier = Modifier.padding(innerPadding).fillMaxSize())
                 }
             }
         }
     }
 }
 
+@Preview
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun BanderaArgentina(modifier: Modifier= Modifier){
+    Column(modifier = Modifier.fillMaxSize()) {
+        Box(
+            modifier = Modifier.weight(1f).
+            fillMaxWidth().
+            background(Color(0xFF74ACDF))
+        )
+        Box(
+            modifier = Modifier.weight(1f).
+            fillMaxWidth().
+            background(Color.White)
+        )
+        Box(
+            modifier = Modifier.weight(1f).fillMaxWidth().background(Color(0xFF74ACDF))
+        )
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    BanderasnvTheme {
-        Greeting("Android")
     }
+
 }
