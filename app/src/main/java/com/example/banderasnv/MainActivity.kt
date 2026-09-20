84e9902c56a1a89204134911f3b9260171e20408
josue <josue2017ivan@gmail.com>
2026-09-20 16:07:24 -0600

    cambios terminados de la bandera de argentina

--- app/src/main/java/com/example/banderasnv/MainActivity.kt
+++ app/src/main/java/com/example/banderasnv/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -11,13 +12,17 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.banderasnv.ui.theme.BanderasnvTheme
 
 class MainActivity : ComponentActivity() {
@@ -46,12 +51,17 @@ fun BanderaArgentina(modifier: Modifier= Modifier){
         Box(
             modifier = Modifier.weight(1f).
             fillMaxWidth().
-            background(Color.White)
-        )
+            background(Color.White), contentAlignment = Alignment.Center
+        ){
+            Image(
+                painter = painterResource(id = R.drawable.argentina_escudo),
+                contentDescription = "esucudo de argentina",
+                modifier = Modifier.size(100.dp)
+            )
+        }
         Box(
             modifier = Modifier.weight(1f).fillMaxWidth().background(Color(0xFF74ACDF))
         )
-
     }
 
 }
