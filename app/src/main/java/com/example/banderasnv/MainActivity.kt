458ddd509d912d36011d67d77f5b8bdacdc8e8c9
josue <josue2017ivan@gmail.com>
2026-09-19 19:23:05 -0600

    bandera terminada de colombia

--- app/src/main/java/com/example/banderasnv/MainActivity.kt
+++ app/src/main/java/com/example/banderasnv/MainActivity.kt
@@ -4,12 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.banderasnv.ui.theme.BanderasnvTheme
 
@@ -20,28 +26,32 @@ class MainActivity : ComponentActivity() {
         setContent {
             BanderasnvTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    BanderaColombia(modifier = Modifier.padding(innerPadding).fillMaxSize())
                 }
             }
         }
     }
 }
 
+@Preview
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    BanderasnvTheme {
-        Greeting("Android")
+fun BanderaColombia(modifier: Modifier = Modifier){
+    Column(modifier = Modifier.fillMaxSize()) {
+        Box(
+            modifier = Modifier.weight(2f).
+            fillMaxWidth().
+            background(Color(0xFFFFCE00))
+        )
+        Box(
+            modifier = Modifier.weight(1f).
+            fillMaxWidth().
+            background(Color(0xFF00247E))
+        )
+        Box(
+            modifier = Modifier.weight(1f).
+            fillMaxWidth().
+            background(Color(0xFFE80351))
+        )
     }
+
 }
